a1e800a57660bc7269dcb9474c168c01a3e02113
Tony Kharioki <antolisah@gmail.com>
2024-09-18 20:58:58 +0300

    refactor unused routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Stack } from 'expo-router';
-// import Colors from '@/constants/Colors';
 
 export default function TabLayout() {
   return (
@@ -11,36 +10,6 @@ export default function TabLayout() {
           headerShown: false,
         }}
       />
-      {/* <Stack.Screen
-        name="create"
-        options={{
-          headerTransparent: true,
-          headerStyle: {
-            backgroundColor: 'rgba(0,0,0,0.4)',
-          },
-          headerTintColor: Colors.palette.text,
-          headerTitleAlign: 'center',
-        }}
-      />
-      <Stack.Screen
-        name="[event]"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="play"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="search"
-        options={{
-          headerTransparent: true,
-          headerTintColor: Colors.palette.text,
-        }}
-      /> */}
     </Stack>
   );
 }
